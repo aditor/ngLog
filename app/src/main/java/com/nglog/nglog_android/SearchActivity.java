db70f3c67696a8e743396de753de3d6baa10e59c
Aditya Kamath <aditya.24@hotmail.com>
2016-09-24 18:41:44 -0700

    Search functionality added

--- app/src/main/java/com/nglog/nglog_android/SearchActivity.java
+++ app/src/main/java/com/nglog/nglog_android/SearchActivity.java
@@ -2,9 +2,15 @@ package com.nglog.nglog_android;
 
 import android.app.ActionBar;
 import android.app.Dialog;
+import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +18,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -21,14 +29,14 @@ import java.util.List;
 /**
  * Created by Adi on 2016-09-24.
  */
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     final List<String> searchList = new ArrayList<String>();   //list of serialnumbers/Location
 
     private Button searchbutton;
 
     private Context context = this;
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
@@ -37,6 +45,11 @@ public class SearchActivity extends AppCompatActivity {
          setSupportActionBar(my_toolbar);
          getSupportActionBar().setTitle(R.string.my_tb_title);
 
+         Intent searchIntent = getIntent();
+            if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())){
+                String query = searchIntent.getStringExtra(SearchManager.QUERY);
+                Toast.makeText(SearchActivity.this, query, Toast.LENGTH_SHORT).show();
+            }
     }
 
     @Override
@@ -44,8 +57,52 @@ public class SearchActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
 
-        return super.onCreateOptionsMenu(menu);
+        /*SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);*/
+
+
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
     }
+
+    @Override
+    //on choosing an option from dropdown list
+    public boolean onQueryTextSubmit(String query) {
+        // User pressed the search button
+        return false;
+    }
+
+    //as you type it changes
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        // User changed the text
+        return false;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Searching by: "+ query, Toast.LENGTH_SHORT).show();
+
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            String uri = intent.getDataString();
+            Toast.makeText(this, "Suggestion: "+ uri, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
 
 }
