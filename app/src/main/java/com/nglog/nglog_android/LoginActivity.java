10959c03cf6f1fd175230335a1577ea6d3594a80
Aditya Kamath <aditya.24@hotmail.com>
2016-09-24 19:26:22 -0700

    new stuff

--- app/src/main/java/com/nglog/nglog_android/LoginActivity.java
+++ app/src/main/java/com/nglog/nglog_android/LoginActivity.java
@@ -38,7 +38,14 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
+
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    private String BASE = "/home/";
+    private String REG = "register/";
+    private String LOGIN = "login/";
+    private String LOGOUT = "logout/";
+
 
     /**
      * Id to identity READ_CONTACTS permission request.
